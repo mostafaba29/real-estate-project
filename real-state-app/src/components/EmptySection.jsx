a6966139ce3a9ba27d8c5c320976e905c73d2312
rawankamal <rwankamal@gmail.com>
2024-12-17 22:22:06 +0200

    fixed stage-one

--- real-state-app/src/components/EmptySection.jsx
+++
@@ -1,4 +0,0 @@
-import '../assets/styles/pages/contactPage.style.scss'
-export default function EmptySection (){
-    return (<section className="empty-section"></section>)
-}
--- real-state-app/src/components/MapSection.jsx
+++
@@ -1,8 +0,0 @@
-import '../assets/styles/pages/contactPage.style.scss'
-export default function MapSection (){
-    return (
-        <section className="map-section">
-        <img src="./assets/imgs/map.jpg" alt="map" className="map" />
-        </section>
-    )
-}
